5d232a878702a8afb639a1a4e28efdd798182471
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2022-12-12 22:25:49 +0100

    Update ExtraStorage.sol

--- ExtraStorage.sol
+++ ExtraStorage.sol
@@ -2,6 +2,8 @@
 
 pragma solidity ^0.8.13;
 
+import "./SimpleStorage.sol";
+
 contract ExtraStorage is SimpleStorage {
     function store(uint _favoriteNumber) public override {
         favoriteNumber = _favoriteNumber + 5;
