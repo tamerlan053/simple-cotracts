5e7a9a57624a03fa287efe2a626777bea24b4764
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2023-01-19 22:07:31 +0100

    Update ExampleMappingStruct.sol

--- ExampleMappingStruct.sol
+++ ExampleMappingStruct.sol
@@ -1,13 +1,13 @@
 //SPDX-License-Identifier: MIT 
 
-pragma solidity ^0.8.15; 
+pragma solidity 0.8.15;
 
 contract ExampleMappingStruct {
 
     struct Transaction {
         uint amount;
         uint timestamp;
-    } 
+    }
 
     struct Balance {
         uint totalBalance;
@@ -15,32 +15,29 @@ contract ExampleMappingStruct {
         mapping(uint => Transaction) deposits;
         uint numWithdrawals;
         mapping(uint => Transaction) withdrawals;
-    } 
-
-    mapping(address => Balance) public balanceReceived;
-
-
-    function getBalance(address _addr) public view returns(uint) {
-        return balanceReceived[_addr].totalBalance; 
     }
 
-    function depositMoney() public payable {
-        balanceReceived[msg.sender].totalBalance += msg.value;
+    mapping(address => Balance) public balances;
 
-        Transaction memory deposit = Transaction(msg.value, block.timestamp);
-        balanceReceived[msg.sender].deposits[balanceReceived[msg.sender].numDeposits] = deposit;
-        balanceReceived[msg.sender].numDeposits++;
+    function getDepositNum(address _from, uint _numDeposit) public view returns(Transaction memory) {
+        return balances[_from].deposits[_numDeposit];
+    }
+ 
+    function depositMoney() public payable {
+        balances[msg.sender].totalBalance += msg.value;
+
+        Transaction memory deposit = Transaction (msg.value, block.timestamp);
+        balances[msg.sender].deposits[balances[msg.sender].numDeposits] = deposit;
+        balances[msg.sender].numDeposits++;
     }
 
     function withdrawMoney(address payable _to, uint _amount) public {
-        balanceReceived[msg.sender].totalBalance -= _amount; //reduce the balance by the amount ot withdraw
+        balances[msg.sender].totalBalance -= _amount;
 
-        //record a new withdrawal
-        Transaction memory withdrawal = Transaction(msg.value, block.timestamp);
-        balanceReceived[msg.sender].withdrawals[balanceReceived[msg.sender].numWithdrawals] = withdrawals;
-        balanceReceived[msg.sender].numWithdrawals++; 
+        Transaction memory withdrawal = Transaction (_amount, block.timestamp);
+        balances[msg.sender].withdrawals[balances[msg.sender].numWithdrawals] = withdrawal;
+        balances[msg.sender].numWithdrawals++;
 
-        //send the amount out.
-        _to.transfer(_amount);
+        _to.transfer(_amount); 
     }
 }
