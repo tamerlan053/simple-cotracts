52f139718e4f020226b0cf8533ce639afe74ca9b
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2023-01-29 21:02:26 +0100

    Update ABIEncode.sol

--- ABIEncode.sol
+++ ABIEncode.sol
@@ -8,11 +8,6 @@ contract Token {
     function transfer(address, uint) external {}
 }
 
-contract AbiEncode {
-    function test(address _contract, bytes calldata data) external {
-        (bool ok, ) = _contract.call(data);
-        require(ok, "call failed");
-    }
 
     function encodeWithSignature(address to, uint amount)
         external
