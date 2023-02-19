9418f0949e9100dcb4ced5fccdb0fc4c0c4d881e
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2023-02-19 20:41:17 +0100

    Add files via upload

---
+++ SendTransferExample.sol
@@ -0,0 +1,38 @@
+//SPDX-License-Identifier: MIT 
+
+pragma solidity 0.8.15;
+
+contract Sender {
+    receive() external payable {}
+
+    function withdrawTransfer(address payable user) public {
+        user.transfer(10);
+    }
+
+    function withdrawSend(address payable user) public {
+        bool isSent = user.send(10);
+
+        require(isSent, "The transcation was unsuccesful");
+    }
+}
+
+contract ReceiverNoAction {
+
+    function balance() public view returns(uint) {
+        return address(this).balance;
+    }
+
+    receive() external payable{}
+}
+
+contract ReceiverAction {
+    uint public balanceReceived;
+
+    receive() external payable {
+        balanceReceived += msg.value;
+    }
+
+    function balance() public view returns(uint) {
+        return address(this).balance;
+    }
+}
