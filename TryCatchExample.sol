4cc6ddca0d60cdd1545a456fdb01b3d3357b14e7
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2023-02-19 16:45:23 +0100

    Add files via upload

---
+++ TryCatchExample.sol
@@ -0,0 +1,30 @@
+//SPDX-License-Identifier: MIT 
+
+pragma solidity 0.8.15;
+
+contract WillThrow {
+    error NotAllowedError(string);
+    function aFunction() public pure {
+        //require(false, "ErrorMessage")
+        //assert(false)
+        revert NotAllowedError("Error message");
+    }
+}
+
+contract ErrorHandling {
+    event ErrorLogging(string reason);
+    event ErrorLogCode(uint errorCode);
+    event ErrorLogBytes(bytes lowLevelData);
+    function catchTheError() public {
+        WillThrow will = new WillThrow();
+        try will.aFunction() {
+            //add code here
+        } catch Error(string memory reason) {
+            emit ErrorLogging(reason);
+        } catch Panic(uint errorCode) {
+            emit ErrorLogCode(errorCode);
+        } catch (bytes memory lowLevelData) {
+            emit ErrorLogBytes(lowLevelData);
+        }
+    }
+}
