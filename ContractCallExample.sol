f811a951d4c2e4cf8b4aac9fd82eb8ecb3837f06
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2023-03-02 22:30:12 +0100

    Update ContractCallExample.sol

--- ContractCallExample.sol
+++ ContractCallExample.sol
@@ -14,12 +14,4 @@ contract ContractOne {
     }
 }
 
-contract ContractTwo {
-    receive() external payable {}
 
-    function depositOnContractOne(address _contractOne) public {
-        //bytes memory payload = abi.encodeWithSignature("deposit()");
-        (bool success, ) = _contractOne.call{value:10, gas: 100000}("");
-        require(success);
-    }
-}
