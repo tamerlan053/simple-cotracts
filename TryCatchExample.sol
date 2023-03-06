bd855bf1a35a33f4173bef69673bcd6e90ffdbab
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2023-03-06 23:46:34 +0100

    Update TryCatchExample.sol

--- TryCatchExample.sol
+++ TryCatchExample.sol
@@ -11,5 +11,22 @@ contract WillThrow {
     }
 }
 
+contract ErrorHandling {
+    event ErrorLogging(string reason);
+    event ErrorLogCode(uint errorCode);
+    event ErrorLogBytes(bytes lowLevelData);
+    function catchTheError() public {
+        WillThrow will = new WillThrow();
+        try will.aFunction() {
+            //add code here!
+        } catch Error(string memory reason) {
+            emit ErrorLogging(reason);
+        } catch Panic(uint errorCode) {
+            emit ErrorLogCode(errorCode);
+        } catch (bytes memory lowLevelData) {
+            emit ErrorLogBytes(lowLevelData);
+        }
+    }
+}
 
 
