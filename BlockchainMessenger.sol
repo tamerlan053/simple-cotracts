98b9e02a7b118f81980bcdd91ad236a399a661e2
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2023-03-15 21:56:17 +0100

    Update BlockchainMessenger.sol

--- BlockchainMessenger.sol
+++ BlockchainMessenger.sol
@@ -9,8 +9,10 @@ contract TheBlockchainMessenger {
 
     string public theMessage;
 
-  
-
-   
+    function updateTheMessage (string memory _newMessage) public {
+        if (msg.sender == owner) {
+            theMessage = _newMessage;
+            changeCounter++;
+        }
     }
 }
