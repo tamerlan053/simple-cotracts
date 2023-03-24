9fd6874d1d44689ce988d88c30c528511a856760
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2023-03-24 23:18:01 +0100

    Update BasicEtherWallet.sol

--- BasicEtherWallet.sol
+++ BasicEtherWallet.sol
@@ -11,10 +11,7 @@ contract EtherWallet {
 
     receive() external payable {}
 
-    function withdraw(uint _amount) external {
-        require(msg.sender == owner, "caller is not owner");
-        payable(msg.sender).transfer(_amount);
-    }
+
 
     function getBalance() external view returns (uint) {
         return address(this).balance;
