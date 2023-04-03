eeac50af954c470cc3eb392fd5a829aad96a4f9f
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2023-04-03 23:27:55 +0200

    Update TryCatchExample.sol

--- TryCatchExample.sol
+++ TryCatchExample.sol
@@ -2,6 +2,14 @@
 
 pragma solidity 0.8.15;
 
+contract WillThrow {
+    error NotAllowedError(string);
+    function aFunction() public pure {
+        //require(false, "ErrorMessage")
+        //assert(false)
+        revert NotAllowedError("Error message");
+    }
+}
 
 contract ErrorHandling {
     event ErrorLogging(string reason);
