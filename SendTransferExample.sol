8e0dc1e76ae326b84f2bbe643b20efa1cf73839f
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2023-04-10 11:41:22 +0300

    Update SendTransferExample.sol

--- SendTransferExample.sol
+++ SendTransferExample.sol
@@ -2,19 +2,6 @@
 
 pragma solidity 0.8.15;
 
-contract Sender {
-    receive() external payable {}
-
-    function withdrawTransfer(address payable user) public {
-        user.transfer(10);
-    }
-
-    function withdrawSend(address payable user) public {
-        bool isSent = user.send(10);
-
-        require(isSent, "The transcation was unsuccesful");
-    }
-}
 
 contract ReceiverAction {
     uint public balanceReceived;
