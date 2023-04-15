d994650f4905766c1fdf02e242849cdf415edc47
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2023-04-15 18:10:07 +0300

    Update ExampleExceptionRequire.sol

--- ExampleExceptionRequire.sol
+++ ExampleExceptionRequire.sol
@@ -9,9 +9,5 @@ contract ExampleExceptionRequire {
         balanceReceived[msg.sender] += msg.value;
     }
 
-    function withdrawMoney(address payable _to, uint _amount) public {
-        require(_amount <= balanceReceived[msg.sender], "Not enough assets!");
-        balanceReceived[msg.sender] -= _amount;
-        _to.transfer(_amount);
-    }
+
 }
