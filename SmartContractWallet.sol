fd7e7835aa9fb89149296ebc5bcb32ecf9f3646b
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2023-05-04 23:44:59 +0200

    Update SmartContractWallet.sol

--- SmartContractWallet.sol
+++ SmartContractWallet.sol
@@ -5,12 +5,18 @@ pragma solidity 0.8.16;
 contract SmartWallet {
     address payable owner;
 
+    mapping(address => uint) public allowance;
+    mapping(address => uint) public isAllowedToSend;
+
     constructor() {
         owner = payable(msg.sender);
     }
 
     function transfer(address payable _to, uint _amount, bytes memory _payload) public returns(bytes memory) {
-        require (msg.sender == owner, "You are not the owner, aborting!");
+        //require (msg.sender == owner, "You are not the owner, aborting!");
+        if(msg.sender != owner) {
+            require(isAllowedToSend[msg.sender], "You are not allowed to send anything from this contract, aboting");
+        }
 
         (bool success, bytes memory returnData) = _to.call{value: _amount}(_payload);
         require (success, "Oops, calle was not successful");
