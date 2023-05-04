b9cf0d89bc31e8333fdb5eba04b89f223dbcb35e
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2023-05-04 23:03:03 +0200

    Update SmartContractWallet.sol

--- SmartContractWallet.sol
+++ SmartContractWallet.sol
@@ -1,7 +1,21 @@
 //SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.15;
+pragma solidity 0.8.16;
 
 contract SmartWallet {
+    address payable owner;
 
+    constructor() {
+        owner = payable(msg.sender);
+    }
+
+    function transfer(address payable _to, uint _amount, bytes memory _payload) public returns(bytes memory) {
+        require (msg.sender == owner, "You are not the owner, aborting!");
+
+        (bool success, bytes memory returnData) = _to.call{value: _amount}(_payload);
+        require (success, "Oops, calle was not successful");
+        return returnData;
+    }
+
+    receive() external payable{}
 }
