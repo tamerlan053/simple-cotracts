5cac55d6d00f0282f9340ed5bc25b0eaa3aab480
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2023-05-06 22:56:52 +0200

    Update BasicForLoop.sol

--- BasicForLoop.sol
+++ BasicForLoop.sol
@@ -3,7 +3,15 @@
 pragma solidity ^0.8.14;
 
 contract Loop {
-
+        function loop() public {
+        for (uint i = 0; i < 10; i++) {
+            if (i == 3) {
+                continue;
+            }
+            if (i == 5) {
+                break;
+            }
+        }
 
         uint j;
         while (j < 10) {
