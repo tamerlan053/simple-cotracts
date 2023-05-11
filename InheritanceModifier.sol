86bb544ccfaaf6f0b93fc80d618224179cddffcf
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2023-05-11 20:30:17 +0200

    Update InheritanceModifier.sol

--- InheritanceModifier.sol
+++ InheritanceModifier.sol
@@ -1,1 +1,16 @@
+//SPDX-License-Identifier: MIT 
 
+pragma solidity 0.8.15;
+
+contract InheritanceModifier {
+    mapping (address => uint) public tokenBalance; 
+
+    address owner; 
+
+    uint tokenPrice = 1 ether; 
+
+    constructor() {
+        owner = msg.sender; 
+        tokenBalance[owner] = 100;
+    }
+}
