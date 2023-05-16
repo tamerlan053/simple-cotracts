9ec974e32f8610716027f629e2616a190d2c406c
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2023-05-16 23:38:44 +0200

    Create SampleVerification.sol

--- SampleVerification.sol
+++ SampleVerification.sol
@@ -3,5 +3,18 @@
 pragma solidity 0.8.15;
 
 contract MyContract {
-    
+    mapping (address => uint) public balance;
+
+    constructor() {
+        balance[msg.sender] = 100;
+    }
+
+    function transfer(address _to, uint amount) public {
+        balance[msg.sender] -= amount;
+        balance[_to] += amount;
+    }
+
+    function someCrypticFunctionName(address _addr) public view returns(uint) {
+        return balance[_addr];
+    }
 }
