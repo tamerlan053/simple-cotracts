742891350b0a3a7c44403ca0ab7dd299a6287275
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2023-05-17 15:19:36 +0200

    Update TokenSale.sol

--- TokenSale/TokenSale.sol
+++ TokenSale/TokenSale.sol
@@ -1,1 +1,8 @@
+//SPDX-License-Identifier: MIT
 
+pragma solidity 0.8.16;
+
+interface IERC20 {
+    function transfer(address to, uint amount) external;
+    function decimals() external view returns(uint);
+}
