2cd09c8caadce98abf7db93a6ffcb7426a3a7301
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2023-08-01 23:48:34 +0200

    Update StructMapping.sol

--- StructMapping.sol
+++ StructMapping.sol
@@ -5,7 +5,9 @@ pragma solidity 0.8.15;
 contract StructMapping {
     mapping (address => uint) public balance;
 
+
     address public owner = msg.sender;
+    address payable nextOwner;
 
     function contractBalance() public view returns(uint) {
         return address(this).balance;
