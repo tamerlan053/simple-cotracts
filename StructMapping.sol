5aefb89545657fcd1fdfcaff776dfcae260c8b61
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2023-08-13 23:40:04 +0200

    Update StructMapping.sol

--- StructMapping.sol
+++ StructMapping.sol
@@ -3,16 +3,5 @@
 pragma solidity 0.8.17;
 
 contract StructMapping {
-    
-    function transfer() public {
-        require(msg.sender == owner, 'You are not the owner');
-    }
 
-   mapping (uint => bool) public allowed;
- 
-    address payable nextOwner;
-
-    constructor() {
-        owner = msg.sender;
-    }
  }
