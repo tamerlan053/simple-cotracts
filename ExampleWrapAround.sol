67254137d8649842e732626e4e0922cfa8149f4a
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-09-14 22:10:20 +0200

    Update ExampleWrapAround.sol

--- ExampleWrapAround.sol
+++ ExampleWrapAround.sol
@@ -1,1 +1,23 @@
+//SPDX-License-Identifier: MIT
 
+pragma solidity 0.8.15;
+
+contract ExampleWrapAround {
+    uint256 public myUint;
+
+    uint8 public myUint8 = 250;
+
+    function setMyUint(uint _myUint) public {
+        myUint = _myUint;
+    }
+
+    function decrementUint() public {
+        unchecked { // update from 0.8.15
+            myUint--;
+        }
+    }
+
+    function icremenentUint8() public {
+        myUint8++;
+    }
+}
