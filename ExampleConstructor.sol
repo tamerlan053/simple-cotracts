0dbbdb7deba93d87605fb12db118c0fdcb1f523a
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-09-16 22:39:30 +0200

    Update ExampleConstructor.sol

--- ExampleConstructor.sol
+++ ExampleConstructor.sol
@@ -1,1 +1,19 @@
+// SPDX-License-Identifier: MIT
 
+pragma solidity 0.8.15;
+
+contract ExampleConstructor {
+    address public myAddress;
+
+    constructor (address _someAddress) {
+        myAddress = _someAddress;
+    }
+
+    function setMyAddress(address _myAddress) public {
+        myAddress = _myAddress;
+    }
+
+    function setAddressToMsgSender() public {
+        myAddress = msg.sender;
+    }
+}
