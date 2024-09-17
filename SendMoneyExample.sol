08c6c8fbf95d877a1809379948ce807d54a44e38
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-09-17 23:15:02 +0200

    Update SendMoneyExample.sol

--- SendMoneyExample.sol
+++ SendMoneyExample.sol
@@ -1,1 +1,11 @@
+//SPDX-License-Identifier: MIT
 
+pragma solidity 0.8.15;
+
+contract SendWithdrawMoney {
+    uint public balanceReceived;
+    
+    function deposit() public payable {
+        
+    }
+}
