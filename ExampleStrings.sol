591b7943fe0c416ab92a6588c4b2344bf62231fa
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-09-19 22:53:11 +0200

    Update ExampleStrings.sol

--- ExampleStrings.sol
+++ ExampleStrings.sol
@@ -1,1 +1,12 @@
+//SPDX-License-Identifier: MIT
 
+pragma solidity 0.8.15;
+
+contract ExampleStrings {
+    string public myString = "Hello World";
+    bytes public myBytes = "Hello World";
+
+    function setMyString(string memory _myString) public {
+        myString = _myString;
+    }
+}
