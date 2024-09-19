da8166a99ba257a245c49ad78c1c3311638b1de0
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-09-19 22:53:23 +0200

    Update ExampleStrings.sol

--- ExampleStrings.sol
+++ ExampleStrings.sol
@@ -9,4 +9,8 @@ contract ExampleStrings {
     function setMyString(string memory _myString) public {
         myString = _myString;
     }
+
+    function compareTwoStrings(string memory _myString) public view returns(bool) {
+        return keccak256(abi.encodePacked(myString)) == keccak256(abi.encodePacked(_myString));
+    }
 }
