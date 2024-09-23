559ba86e78df898e1a2bb2f88e7e95e1d7895d46
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-09-23 23:55:20 +0200

    Update ExampleMapping.sol
    mapping for address added

--- ExampleMapping.sol
+++ ExampleMapping.sol
@@ -4,8 +4,13 @@ pragma solidity ^0.8.26;
 
 contract ExampleMapping {
     mapping(uint => bool) public myMapping;
+    mapping(address => bool) public myAddressMapping;
 
     function setValue(uint _index) public {
         myMapping[_index] = true;
+    }
+
+    function setMyAddressToTrue() public {
+        myAddressMapping[msg.sender] = true;
     }
 }
