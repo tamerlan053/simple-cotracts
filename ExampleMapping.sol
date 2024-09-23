78251b5777dccac26bf648a951481d131c51e091
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-09-23 23:51:13 +0200

    Update ExampleMapping.sol
    mapping added

--- ExampleMapping.sol
+++ ExampleMapping.sol
@@ -3,5 +3,9 @@
 pragma solidity ^0.8.26;
 
 contract ExampleMapping {
-    
+    mapping(uint => bool) public myMapping;
+
+    function setValue(uint _index) public {
+        myMapping[_index] = true;
+    }
 }
