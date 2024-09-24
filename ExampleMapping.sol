408d1adecb06360d670ceba2913ef1de35af80fa
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-09-24 23:31:46 +0200

    Update ExampleMapping.sol
    mapping to mapping

--- ExampleMapping.sol
+++ ExampleMapping.sol
@@ -5,6 +5,7 @@ pragma solidity ^0.8.26;
 contract ExampleMapping {
     mapping(uint => bool) public myMapping;
     mapping(address => bool) public myAddressMapping;
+    mapping(uint => mapping(uint => bool)) public uintUintBoolMapping;
 
     function setValue(uint _index) public {
         myMapping[_index] = true;
@@ -12,5 +13,9 @@ contract ExampleMapping {
 
     function setMyAddressToTrue() public {
         myAddressMapping[msg.sender] = true;
+    }
+
+    function setUintUintBoolMapping(uint _key1, uint _key2, bool _value) public {
+        uintUintBoolMapping[_key1][_key2] = _value;
     }
 }
